fc9a5bb975d800961bcc7a557d78f5623b7f4c84
WHITEROSE <firasmoussa60@gmail.com>
2026-09-06 20:27:58 +0200

    load project by id

--- src/app.js
+++ src/app.js
@@ -1,17 +1,46 @@
-fetch("data.json")
-    .then(response => {
-        if (!response.ok) {
-            throw Error("Failed to read file");
-        }
-        return response.json();
-    })
-    .then((projects) => {
-        const grid = document.getElementById("project-grid");
-        if (!grid) return;
+async function loadAllProjects() {
+    fetch("data.json")
+        .then(response => {
+            if (!response.ok) {
+                throw Error("Failed to read file");
+            }
+            return response.json();
+        })
+        .then((projects) => {
+            mapProjectsToCards(projects);
+        })
+        .catch(error => console.error("Faield to fetch data: ", error));
+}
 
-        grid.innerHTML = projects
-            .map(
-                (project) => `
+async function loadProjectById(id) {
+    if (id != Number) console.error("Invalid id type");
+
+    fetch("data.json")
+        .then((res) => res.json())
+        .then((projects) => {
+            const project = projects.find((p) => p.id === id);
+
+            if (!project) {
+                detail.innerHTML = "<p>Project not found.</p>";
+                return;
+            }
+
+            detail.innerHTML = `
+                <p>${project.category} | ${project.year}</p>
+                <h1>${project.title}</h1>
+                <p>${project.summary}</p>
+                <p>${project.description}</p>
+            `;
+        });
+}
+
+async function mapProjectsToCards(projects) {
+    const grid = document.getElementById("project-grid");
+    if (!grid) return;
+
+    grid.innerHTML = projects
+        .map(
+            (project) => `
             <article class="project-card">
                 <h3>${project.title}</h3>
                 <p>${project.category} | ${project.year}</p>
@@ -19,7 +48,14 @@ fetch("data.json")
                 <a href="project.html?id=${project.id}">View project</a>
             </article>
             `
-            )
-            .join("");
-    })
-    .catch(error => console.error("Faield to fetch data: ", error));
+        )
+        .join("");
+}
+
+const params = new URLSearchParams(window.location.search);
+const id = Number(params.get("id"));
+
+const detail = document.getElementById("project-detail");
+
+if (document.getElementById("project-grid")) loadAllProjects();
+if (document.getElementById("project-detail")) loadProjectById(id);
